605a618556887aeaf0633b75aa052fdb76987cf4
Lukelme <Lukelmemonteiro88@gmail.com>
2023-12-05 10:40:29 -0300

    ex-3

---
+++ react-3/src/App.js
@@ -0,0 +1,8 @@
+import respostamensagem from './funcao/mensagem';
+import './App.css';
+
+function App() {
+  return respostamensagem ();
+}
+
+export default App;
---
+++ react-3/src/funcao/mensagem.js
@@ -0,0 +1,9 @@
+import './mensagem.css'
+function respostamensagem(){
+    return(
+        <div class="div1">
+            <h1>oi meu nome é lukelme </h1>
+        </div>
+    );
+}
+export default respostamensagem;
